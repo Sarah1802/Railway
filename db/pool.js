0102feafed8075bccc70b38184a05f923e4c8656
Sarah <Sarah.Liu.1802@gmail.com>
2025-10-29 21:28:45 -0700

    changing connection pool to connection string

--- db/pool.js
+++ db/pool.js
@@ -1,9 +1,10 @@
 const { Pool } = require('pg');
 
 module.exports = new Pool({
-  host: "localhost", // or wherever the db is hosted
-  user: "sarahliu",
-  database: "top_users",
-  password: "<role_password>",
-  port: 5432 // The default port
+//   host: "localhost", // or wherever the db is hosted
+//   user: "",
+//   database: "top_users",
+//   password: "<role_password>",
+//   port: 5432 // The default port
+    connectionString: process.env.DATABASE_URL
 });
